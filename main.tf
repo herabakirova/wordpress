6f3871ea9c29ccc471c14561172082d0cf5c6e47
Aigerim Bakirova <153569566+herabakirova@users.noreply.github.com>
2024-05-04 16:35:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = var.region
+  region     = var.region
+  access_key = "????"
+  secret_key = "????"
 }
 
 resource "aws_vpc" "vpc" {
@@ -7,13 +9,15 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "public1" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.cidr_public1
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.cidr_public1
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.cidr_public2
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.cidr_public2
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private1" {
@@ -79,6 +83,7 @@ resource "aws_route_table_association" "d" {
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
+  vpc_id      = aws_vpc.vpc.id
   description = "Allow TLS inbound traffic"
 
   ingress {
@@ -106,7 +111,8 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_security_group" "deny_all" {
-  name = "db"
+  vpc_id = aws_vpc.vpc.id
+  name   = "db"
   ingress {
     from_port   = 3306
     to_port     = 3306
@@ -143,12 +149,32 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "database" {
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = var.instance_type
-  subnet_id       = aws_subnet.private1.id
-  security_groups = aws_security_group.deny_all.id
-  user_data       = file(var.userdata)
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage      = 10
+  db_name                = "mydb"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.deny_all.id]
+  username               = var.db_username
+  password               = var.db_password
+  skip_final_snapshot    = true
+}
+
+output "db_endpoint" {
+  value       = aws_db_instance.mysql.endpoint
+  description = "Endpoint of the MySQL DB"
 }
 
 resource "aws_instance" "web" {
@@ -156,10 +182,44 @@ resource "aws_instance" "web" {
   instance_type   = var.instance_type
   subnet_id       = aws_subnet.public1.id
   key_name        = aws_key_pair.deployer.key_name
-  security_groups = aws_security_group.allow_tls.id
+  security_groups = [aws_security_group.allow_tls.id]
   user_data       = file(var.userdata)
   depends_on = [
-    aws_instance.database
+    aws_db_instance.mysql
   ]
 }
+resource "null_resource" "wp" {
+  depends_on = [
+    aws_instance.web
+  ]
+  connection {
+    host        = aws_instance.web.public_ip
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+  }
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update && sudo apt upgrade -y",
+      "sudo apt install -y apache2 php libapache2-mod-php php-mysql",
+      "sudo systemctl enable apache2",
+      "sudo systemctl start apache2",
+      "cd /var/www/html",
+      "sudo rm index.html",
+      "sudo wget https://wordpress.org/latest.tar.gz",
+      "sudo tar -xzf latest.tar.gz",
+      "sudo mv wordpress/* .",
+      "sudo rm -rf wordpress latest.tar.gz",
+      "sudo chown -R www-data:www-data /var/www/html",
+      "sudo find /var/www/html -type d -exec chmod 755 {} \\;",
+      "sudo find /var/www/html -type f -exec chmod 644 {} \\;",
+      "sudo cp wp-config-sample.php wp-config.php",
+      "sudo sed -i 's/database_name_here/${aws_db_instance.mysql.db_name}/g' wp-config.php",
+      "sudo sed -i 's/username_here/${var.db_username}/g' wp-config.php",
+      "sudo sed -i 's/password_here/${var.db_password}/g' wp-config.php",
+      "sudo sed -i 's/localhost/${aws_db_instance.mysql.endpoint}/g' wp-config.php",
+      "sudo systemctl restart apache2"
+    ]
+  }
+}
